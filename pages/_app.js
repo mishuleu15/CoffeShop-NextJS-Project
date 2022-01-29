f68bac0b214c879f66812930294423c075b32abf
Catalin Mihai <mishu_leu15@yahoo.com>
2022-01-29 22:18:25 +0200

    created global and home css, added footer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2022 SlickRobo</p>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
